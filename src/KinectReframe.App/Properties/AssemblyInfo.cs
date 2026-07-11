60440ece124375645b69a1060a26a82e41b1184e
Nathan <101719115+n4thyan@users.noreply.github.com>
2026-07-11 13:54:07 +0100

    Update product metadata for studio UI milestone

--- src/KinectReframe.App/Properties/AssemblyInfo.cs
+++ src/KinectReframe.App/Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("Kinect Reframe")]
-[assembly: AssemblyDescription("Xbox 360 Kinect tracking and live depth rendering lab")]
+[assembly: AssemblyDescription("Xbox 360 Kinect tracking, depth visualisation and real-time 3D studio")]
 [assembly: AssemblyCompany("n4thyan")]
 [assembly: AssemblyProduct("Kinect Reframe")]
-[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyCopyright("Copyright © 2026 Nathan May")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: Guid("9D2B7053-6446-4DBF-A9A3-45A5CC3B6E2B")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
