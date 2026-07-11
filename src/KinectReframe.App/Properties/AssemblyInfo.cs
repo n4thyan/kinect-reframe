70292e5acd04c9830074ebeadb6055a6caa0daa6
Nathan <101719115+n4thyan@users.noreply.github.com>
2026-07-11 12:17:36 +0100

    chore: add assembly metadata

---
+++ src/KinectReframe.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Kinect Reframe")]
+[assembly: AssemblyDescription("Xbox 360 Kinect tracking and live depth rendering lab")]
+[assembly: AssemblyCompany("n4thyan")]
+[assembly: AssemblyProduct("Kinect Reframe")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: Guid("9D2B7053-6446-4DBF-A9A3-45A5CC3B6E2B")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
